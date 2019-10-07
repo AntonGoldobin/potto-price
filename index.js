6586b48cb5570ac60e27151a2edc566ea8e737e6
Goldobin Anton <722451@it-atlantic.com>
2019-10-08 00:03:31 +0300

    comments was added

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const generalUrl = 'https://youthstore.ru'
 
 const url = 'https://youthstore.ru/c/men/clothes/winter-jackets'
 
+//all data from all pages
 let pagesData = []
 
 let nextPageUrl = ''
 
+//infinity cycle while bot can find 'next page' button
 function getAllPages(url){
 
   axios.get(url)
@@ -17,8 +19,10 @@ function getAllPages(url){
       let getData = html => {
         const $ = cheerio.load(html, { decodeEntities: false })
 
+        //data for every page
         let data = []
 
+        //try to find 'next page' button, else put empty string for ending cycle
         if($('#pagination').find(".to-next a").attr('href')){
           nextPageUrl = generalUrl + $('#pagination').find(".to-next a").attr('href')
         }
@@ -26,6 +30,7 @@ function getAllPages(url){
           nextPageUrl = ''
         }
 
+        //find all items and get title, info and price
         $("#items-holder li").each((i, elem) => {
           data.push({
             title: $(elem).find('.title').text(),
@@ -35,12 +40,14 @@ function getAllPages(url){
 
         });
 
-
+        //push current page data to 'allPages' array
         pagesData.push(data)
       }
 
+      //start grabbing
       await getData(responce.data)
 
+      //if bot can find next page, start func again, else show results
       if(nextPageUrl !== ''){
         await getAllPages(nextPageUrl)
       }
@@ -56,12 +63,9 @@ function getAllPages(url){
 
 }
 
-function startGrab(url){
-  getAllPages(url)
-}
+//First start
+getAllPages(url)
 
-
-startGrab(url)
 
 
 
